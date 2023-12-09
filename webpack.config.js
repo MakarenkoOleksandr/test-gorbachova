ed7ada17fade2151d649cc2e0d5b30f26606a7fc
MakarenkoOleksandr <majop11111@gmail.com>
2023-12-09 02:35:59 +0100

    Build

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: () => [autoprefixer()],
+            },
+          },
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [
+    // Твои плагины здесь
+  ],
+  mode: "development",
+  devServer: {
+    contentBase: "./dist",
+  },
+};
